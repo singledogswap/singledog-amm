ccb7c138158694bebcb918876b3ce017927d5c6d
caixiaojian <caifan382933186@gmail.com>
2021-05-24 22:09:43 +0800

    commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 // import { bsc, fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 import { injected, bsc } from '../connectors'
 // TODO
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+export const ROUTER_ADDRESS = '0x0C1FEd0B927877a401Fa287BbF3Ba927C7C6d20C'
 
 // a list of tokens by chain
 type ChainTokenList = {
